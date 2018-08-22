e03cfe00996127c60fabf6403382ed7c2719bfaa
LHWen <1109070804@qq.com>
2018-08-22 11:21:50 +0800

    Home View

---
+++ WSwiftApp/Home/HomeCollectionViewCell.swift
@@ -0,0 +1,85 @@
+//
+//  HomeCollectionViewCell.swift
+//  WSwiftApp
+//
+//  Created by LHWen on 2018/8/22.
+//  Copyright © 2018年 LHWen. All rights reserved.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    
+    private var containerView: UIView?
+    var iconView: UIImageView?
+    var titleLbl: UILabel?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        p_setup()
+    }
+    
+    private func p_setup() {
+        
+        p_setContainerView()
+        p_setIconAndTitleLayout()
+        p_setupleftAndBottomLineView()
+    }
+    
+    private func p_setContainerView() {
+        
+        if containerView == nil {
+            containerView = CreateViewFactory.p_setView(.white, 1.0, 1.0)
+            self.contentView.addSubview(containerView!)
+            containerView?.snp.makeConstraints({ (make) -> Void in
+                make.edges.equalTo(self.contentView)
+            })
+        }
+    }
+    
+    private func p_setIconAndTitleLayout() {
+        
+        if iconView == nil {
+            iconView = CreateViewFactory.p_setImagVIewScaleAspectFit("homepage_menu_icon_orderQuery")
+            containerView?.addSubview(iconView!)
+            iconView?.snp.makeConstraints({ (make) -> Void in
+                make.centerX.equalTo(containerView!.snp.centerX)
+                make.top.equalTo(20.0)
+            })
+            
+            titleLbl = CreateViewFactory.p_setLableWhiteBGAndOneLines("item", 12.0, ColorUtility.colorWithHexString(toConvert: "#333333", a: 1.0), .center)
+            containerView?.addSubview(titleLbl!)
+            titleLbl?.snp.makeConstraints({ (make) -> Void in
+                make.centerX.equalTo(containerView!.snp.centerX)
+                make.bottom.equalTo(-15.0)
+            })
+        }
+    }
+    
+    private func p_setupleftAndBottomLineView() {
+        
+        let lineColor: UIColor = ColorUtility.colorWithHexString(toConvert: "#e5e5e5", a: 1.0)
+        let rightLine = CreateViewFactory.p_setView(lineColor, 1.0, 1.0)
+        let bottomLine = CreateViewFactory.p_setView(lineColor, 1.0, 1.0)
+        
+        containerView?.addSubview(rightLine)
+        containerView?.addSubview(bottomLine)
+        
+        rightLine.snp.makeConstraints { (make) -> Void in
+            make.top.right.equalTo(containerView!)
+            make.bottom.equalTo(containerView!.snp.bottom).offset(-0.5)
+            make.width.equalTo(0.5)
+        }
+        
+        bottomLine.snp.makeConstraints { (make) -> Void in
+            make.left.bottom.right.equalTo(containerView!)
+            make.height.equalTo(0.5)
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
--- WSwiftApp/Home/HomeViewController.swift
+++ WSwiftApp/Home/HomeViewController.swift
@@ -9,11 +9,54 @@
 import UIKit
 
 class HomeViewController: BaseViewController {
+    
+    var flowLayout: UICollectionViewFlowLayout?
+    var collectionView: UICollectionView?
+    let dataArr = ["item 1", "item 2", "item 3", "item 4", "item 5", "item 6", "item 7", "item 8"]
+    let kCell = "CollectionCell"
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        p_setHeaderView()
+        p_setCollectionViewLayout()
+    }
+    
+    private func p_setHeaderView() {
         
+        let hColor: UIColor = ColorUtility.colorWithHexString(toConvert: "#1aa0ed", a: 1.0)
+        let headerView: UIView = CreateViewFactory.p_setView(hColor, 1.0, 1.0)
+        self.view.addSubview(headerView)
+        headerView.snp.makeConstraints { (make) ->Void in
+            make.left.top.right.equalTo(self.view)
+            make.height.equalTo(self.view.bounds.width/2.0)
+        }
+    }
+    
+    func p_setCollectionViewLayout() {
+        
+        if flowLayout == nil {
+            flowLayout = UICollectionViewFlowLayout()
+            flowLayout?.sectionInset = UIEdgeInsetsMake(0, 0, 0, 0)
+            flowLayout?.minimumInteritemSpacing = 0
+            flowLayout?.minimumLineSpacing = 0
+            
+            let collectionVRect: CGRect = CGRect(x: 0, y: self.view.bounds.width/2.0, width: self.view.bounds.width, height: self.view.bounds.height - self.view.bounds.width/2.0 - 49.0)
+            collectionView = UICollectionView(frame: collectionVRect, collectionViewLayout: flowLayout!)
+            collectionView?.backgroundColor = ColorUtility.colorWithHexString(toConvert: "efeff4", a: 1.0)
+            collectionView?.delegate = self
+            collectionView?.dataSource = self
+            collectionView?.register(HomeCollectionViewCell.classForCoder(), forCellWithReuseIdentifier: kCell)
+//            collectionView?.register(UICollectionReusableView.classForCoder(), forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: kHeader)
+//            collectionView?.register(UICollectionReusableView.classForCoder(), forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: kFooter)
+            view.addSubview(collectionView!)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,4 +64,48 @@ class HomeViewController: BaseViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print("data array count is \(String(describing: self.dataArr.count))")
+        return (self.dataArr.count)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell: HomeCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: kCell, for: indexPath) as! HomeCollectionViewCell
+        
+        cell.titleLbl?.text = self.dataArr[indexPath.row]
+        
+        return cell
+    }
+    
+    // 定义大小
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let itemSize: CGSize = CGSize(width: self.view.bounds.width/3.0, height: 89.0);
+        return itemSize;
+    }
+    
+    // 设置是否可以选择cell
+    func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    // 设置是否支持高亮
+    func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    // 选择项
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        print("click collection view item row is \(indexPath.row)")
+    }
 }
